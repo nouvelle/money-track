6f7c3df5c34cbac49a0cb5d38cfdd075b0b68f75
nouvelle <nouvellenuts@gmail.com>
2020-01-04 15:32:43 +0900

    add Uppercase files

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+const Header = () => (
+  <Head>
+    <title>Money Track</title>
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+);
+
+export default Header;
